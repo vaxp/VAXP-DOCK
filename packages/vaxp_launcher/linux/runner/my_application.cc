21b5ffa19eda5769bfc3d14e0f018fc5e2073ab6
hussein <vaxpeco.com>
2025-11-06 02:49:10 +0300

    up

--- packages/vaxp_launcher/linux/runner/my_application.cc
+++ packages/vaxp_launcher/linux/runner/my_application.cc
@@ -4,6 +4,7 @@
 #ifdef GDK_WINDOWING_X11
 #include <gdk/gdkx.h>
 #endif
+#include <gdk/gdkkeysyms.h>
 
 #include "flutter/generated_plugin_registrant.h"
 
@@ -14,81 +15,107 @@ struct _MyApplication {
 
 G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 
-// Called when first Flutter frame received.
-static void first_frame_cb(MyApplication* self, FlView *view)
-{
-  gtk_widget_show(gtk_widget_get_toplevel(GTK_WIDGET(view)));
+// منع الإغلاق من مدير النوافذ
+static gboolean on_delete_event(GtkWidget *widget, GdkEvent *event, gpointer user_data) {
+  return TRUE; // لا تغلق النافذة
 }
 
-// Implements GApplication::activate.
+// إعادة fullscreen إذا خرج منها المستخدم
+static gboolean on_window_state_event(GtkWidget *widget, GdkEventWindowState *event, gpointer user_data) {
+  GtkWindow *window = GTK_WINDOW(widget);
+  if (!(event->new_window_state & GDK_WINDOW_STATE_FULLSCREEN)) {
+    gtk_window_fullscreen(window);
+  }
+  return FALSE;
+}
+
+// منع Alt+F4 و F11
+static gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
+  if ((event->state & GDK_MOD1_MASK) && event->keyval == GDK_KEY_F4) {
+    return TRUE; // منع Alt+F4
+  }
+  if (event->keyval == GDK_KEY_F11) {
+    return TRUE; // منع F11
+  }
+  return FALSE; // السماح بباقي المفاتيح
+}
+
+// بعد أول إطار من Flutter
+static void first_frame_cb(MyApplication* self, FlView *view) {
+  GtkWidget* window = gtk_widget_get_toplevel(GTK_WIDGET(view));
+  gtk_widget_show(window);
+  gtk_window_fullscreen(GTK_WINDOW(window)); // إجبار fullscreen بعد ظهور الإطار
+}
+
+// -------------------------------------------
+//              Main Activate
+// -------------------------------------------
 static void my_application_activate(GApplication* application) {
   MyApplication* self = MY_APPLICATION(application);
-  GtkWindow* window =
-      GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+  GtkWindow* window = GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
   GtkWidget* window_widget = GTK_WIDGET(window);
   gtk_widget_set_app_paintable(window_widget, TRUE);
+
+  // إزالة الديكور + منع التغيير بالحجم
+  gtk_window_set_decorated(window, FALSE);
+  gtk_window_set_resizable(window, FALSE);
+
+  // تمكين الشفافية في النافذة
   GdkScreen* screen = gtk_window_get_screen(window);
-#if GTK_CHECK_VERSION(3, 0, 0)
   GdkVisual* visual = gdk_screen_get_rgba_visual(screen);
   if (visual != nullptr) {
     gtk_widget_set_visual(window_widget, visual);
   }
-#endif
 
+  // ربط الأحداث لمنع الإغلاق والخروج من fullscreen
+  g_signal_connect(window_widget, "delete-event", G_CALLBACK(on_delete_event), NULL);
+  g_signal_connect(window_widget, "window-state-event", G_CALLBACK(on_window_state_event), NULL);
+  g_signal_connect(window_widget, "key-press-event", G_CALLBACK(on_key_press), NULL);
 
-  // Use a header bar when running in GNOME as this is the common style used
-  // by applications and is the setup most users will be using (e.g. Ubuntu
-  // desktop).
-  // If running on X and not using GNOME then just use a traditional title bar
-  // in case the window manager does more exotic layout, e.g. tiling.
-  // If running on Wayland assume the header bar will work (may need changing
-  // if future cases occur).
-  gboolean use_header_bar = TRUE;
-#ifdef GDK_WINDOWING_X11
-  if (GDK_IS_X11_SCREEN(screen)) {
-    const gchar* wm_name = gdk_x11_screen_get_window_manager_name(screen);
-    if (g_strcmp0(wm_name, "GNOME Shell") != 0) {
-      use_header_bar = FALSE;
-    }
-  }
-#endif
-  if (use_header_bar) {
-    GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-    gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "vaxp_launcher");
-    gtk_header_bar_set_show_close_button(header_bar, TRUE);
-    gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
-  } else {
-    gtk_window_set_title(window, "vaxp_launcher");
+  // الحصول على حجم الشاشة الحديثة (بدون deprecated)
+  GdkDisplay* display = gdk_display_get_default();
+  GdkMonitor* monitor = gdk_display_get_primary_monitor(display);
+  if (monitor == NULL) {
+    // fallback إذا لم يوجد monitor رئيسي
+    monitor = gdk_display_get_monitor(display, 0);
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  GdkRectangle geometry;
+  gdk_monitor_get_geometry(monitor, &geometry);
 
+  gtk_window_move(window, geometry.x, geometry.y);
+  gtk_window_set_default_size(window, geometry.width, geometry.height);
+
+  // خلفية Flutter
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
 
   FlView* view = fl_view_new(project);
+
+  // ✅ جعل الخلفية سوداء بنسبة شفافية 30%
   GdkRGBA background_color;
-  // Background defaults to black, override it here if necessary, e.g. #00000000 for transparent.
-    gdk_rgba_parse(&background_color, "#00000000"); // transparent
-    fl_view_set_background_color(view, &background_color);
+  background_color.red = 0.0;
+  background_color.green = 0.0;
+  background_color.blue = 0.0;
+  background_color.alpha = 0.3; // <-- شفافية 30%
+  fl_view_set_background_color(view, &background_color);
+
   gtk_widget_show(GTK_WIDGET(view));
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
 
-  // Show the window when Flutter renders.
-  // Requires the view to be realized so we can start rendering.
+  // عرض النافذة بعد أول إطار من Flutter
   g_signal_connect_swapped(view, "first-frame", G_CALLBACK(first_frame_cb), self);
   gtk_widget_realize(GTK_WIDGET(view));
 
   fl_register_plugins(FL_PLUGIN_REGISTRY(view));
-
   gtk_widget_grab_focus(GTK_WIDGET(view));
 }
 
-// Implements GApplication::local_command_line.
+// -------------------------------------------
+//        باقي دوال التطبيق الافتراضية
+// -------------------------------------------
 static gboolean my_application_local_command_line(GApplication* application, gchar*** arguments, int* exit_status) {
   MyApplication* self = MY_APPLICATION(application);
-  // Strip out the first argument as it is the binary name.
   self->dart_entrypoint_arguments = g_strdupv(*arguments + 1);
 
   g_autoptr(GError) error = nullptr;
@@ -100,29 +127,17 @@ static gboolean my_application_local_command_line(GApplication* application, gch
 
   g_application_activate(application);
   *exit_status = 0;
-
   return TRUE;
 }
 
-// Implements GApplication::startup.
 static void my_application_startup(GApplication* application) {
-  //MyApplication* self = MY_APPLICATION(object);
-
-  // Perform any actions required at application startup.
-
   G_APPLICATION_CLASS(my_application_parent_class)->startup(application);
 }
 
-// Implements GApplication::shutdown.
 static void my_application_shutdown(GApplication* application) {
-  //MyApplication* self = MY_APPLICATION(object);
-
-  // Perform any actions required at application shutdown.
-
   G_APPLICATION_CLASS(my_application_parent_class)->shutdown(application);
 }
 
-// Implements GObject::dispose.
 static void my_application_dispose(GObject* object) {
   MyApplication* self = MY_APPLICATION(object);
   g_clear_pointer(&self->dart_entrypoint_arguments, g_strfreev);
@@ -140,12 +155,7 @@ static void my_application_class_init(MyApplicationClass* klass) {
 static void my_application_init(MyApplication* self) {}
 
 MyApplication* my_application_new() {
-  // Set the program name to the application ID, which helps various systems
-  // like GTK and desktop environments map this running application to its
-  // corresponding .desktop file. This ensures better integration by allowing
-  // the application to be recognized beyond its binary name.
   g_set_prgname(APPLICATION_ID);
-
   return MY_APPLICATION(g_object_new(my_application_get_type(),
                                      "application-id", APPLICATION_ID,
                                      "flags", G_APPLICATION_NON_UNIQUE,
